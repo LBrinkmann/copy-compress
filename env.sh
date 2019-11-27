755f837faf0df65f7a21ed1cbc1a2f70756aa8a4
Levin Brinkmann <com@levin-brinkmann.de>
2019-11-27 10:18:50 +0000

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,6 +1,2 @@
-
-
-export MOVIE_FOLDER_ORIGINAL=/Volumes/space/test
-export MOVIE_FOLDER_LOW=/Users/levin/projects/copy-compress/test/low
-export MOVIE_FOLDER_HIGH=/Users/levin/projects/copy-compress/test/high
-export MOVIE_FOLDER_DATA=/Users/levin/projects/copy-compress/test/data
+export DEVICE_NAMES=L1,L2,B1,B2,S1,S2
+export DEVICE_PATH=/media/ubuntu/
